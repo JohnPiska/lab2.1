943d67c192e52b659d6802ccf182cf73de1e77fa
L <L@L>
2025-03-05 22:19:01 +0500

    Добавьте файлы проекта.

---
+++ lab2.1/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let getLastDigit number =
+    let absNumber = abs number  
+    absNumber % 10              
+
+let rec getNumbersFromUser () =
+    printf "Введите числа через пробел: "
+    let input = Console.ReadLine()
+    
+    try
+        let numberStrings = input.Split(' ')      
+        let numbers = List.map int (Array.toList numberStrings) 
+        numbers 
+    with
+        | :? FormatException -> 
+            printfn "Ошибка: Только числа!"
+            getNumbersFromUser () 
+
+
+let numbers = getNumbersFromUser ()
+let lastDigits = List.map getLastDigit numbers
+
+printfn "Исходный список чисел: %A" numbers
+printfn "Список последних цифр: %A" lastDigits
+
+
